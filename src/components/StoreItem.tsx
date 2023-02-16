0d91a35ae539e76bf20f9ff2b637b77fae61f7c3
Salekin Imran <abir045@gmail.com>
2023-02-16 23:31:22 +0600

    updated store page

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {formatCurrency} from '../utilities/formatCurrency'
 
 type StoreItemProps = {
 
@@ -10,10 +10,37 @@ imgUrl: string
 
 }
 
-const StoreItem = () => {
+export function  StoreItem  ({ id, name , price, imgUrl} : StoreItemProps) 
+  {
+   const quantity = 0 
   return (
-    <div>StoreItem</div>
-  )
-}
+    <div>
+      <div className=''>
+        <img className='w-[350px] h-[300px] object-cover' src={imgUrl} alt={name}/>
+        <div className='flex justify-between'>
+        <h1>{name}</h1>
+        <span>{formatCurrency(price) }</span>
+        </div>
+        <div className='mt-[5%]'>
+          {quantity === 0 ? (
+            <button className='w-full bg-blue-500 rounded text-white'>
+              + Add to cart
+            </button> 
+          ) : <div className='flex flex-col items-center'>
+            
+              <div className='flex justify-center gap-3 mb-3'>
+                <button>-</button>
+                <span>{quantity}</span> in cart
+                <button>+</button>
+              </div>
+              <button className='bg-red-500 rounded text-white p-2'>Remove</button>
+            </div>}
 
-export default StoreItem
+        </div>
+        
+      </div>
+   
+  </div>
+  ) 
+  
+}
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { StoreItem } from '../components/StoreItem'
 import storeItems from '../data/items.json'
 
 const Store = () => {
   return (
-    <div>
-      <h1>Store</h1> 
+    <div className='mx-[5%]'>
+      <h1 className='mb-5'>Store</h1> 
        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-3'>
           {
             storeItems.map((item)=> (
-              <div>
-                {JSON.stringify(item)}
+             <div>
+                <StoreItem {...item}/>
               </div>
             ))
           }
